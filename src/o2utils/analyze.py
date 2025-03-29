dddff30369a57ef1e82b310265f2b4aee7338481
Frederik Krämer <frdrkkraemer@gmail.com>
2025-03-30 00:41:34 +0100

    add fit_all

--- src/o2utils/analyze.py
+++ src/o2utils/analyze.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import NamedTuple, cast
+from typing import Literal, NamedTuple, TypedDict, cast
 
 import polars as pl
 from scipy import stats
@@ -14,15 +14,39 @@ class LinregressResult(NamedTuple):
     intercept_stderr: float
 
 
+class MetadataRow(TypedDict):
+    source_file: str
+    cleaned_source_file: str
+    start_time_newport: str
+    record_type: str
+    female_id: str
+    target_temperature: Literal["0C", "4C"]
+    atm_pressure_mb: float
+    n_eggs: float
+    n_eggs_weighed: float
+    measured_fresh_weight_grams: float
+    adjusted_fresh_weight_grams: float
+    bacteria_group: str
+    vol_respiration_chamber_ml: float
+    analysis_start_seconds: int
+    analysis_stop_seconds: int
+    record_flag: int
+    record_flag_description: str
+    comment: str
+
+
 def linear_fit(
     df: pl.DataFrame,
-    start: int,
-    stop: int,
+    metadata: MetadataRow,
     x_col: str = "time_seconds",
     y_col: str = "oxygen",
     y2_col: str = "temperature",
 ) -> tuple[pl.DataFrame, pl.DataFrame]:
-    name = df.item(0, "source_file")
+    name = df.item(0, "cleaned_source_file")
+
+    start = metadata["analysis_start_seconds"]
+    stop = metadata["analysis_stop_seconds"]
+
     df = df.filter(pl.col(x_col).is_between(start, stop))
 
     result = cast(LinregressResult, stats.linregress(df.get_column(x_col), df.get_column(y_col)))
@@ -64,11 +88,42 @@ def linear_fit(
     return res_df, df
 
 
-def get_fit(source_file: Path, info_file: str) -> tuple[pl.DataFrame, pl.DataFrame]:
-    info = pl.read_excel(info_file, sheet_name="metadata").row(
-        by_predicate=pl.col("cleaned_source_file") == source_file.stem, named=True
+def get_fit(
+    source_file: Path, info_file: Path, start: int | None = None, stop: int | None = None
+) -> tuple[pl.DataFrame, pl.DataFrame]:
+    info = cast(
+        MetadataRow,
+        pl.read_excel(info_file, sheet_name="metadata").row(
+            by_predicate=pl.col("cleaned_source_file") == source_file.stem, named=True
+        ),
     )
 
     data = pl.read_csv(source_file)
 
-    return linear_fit(data, start=info["analysis_start_seconds"], stop=info["analysis_stop_seconds"])
+    start = start or info["analysis_start_seconds"]
+    stop = stop or info["analysis_stop_seconds"]
+
+    return linear_fit(data, info)
+
+
+def fit_all(
+    folder: Path,
+    info_file: Path,
+) -> tuple[pl.DataFrame, pl.DataFrame]:
+    info_df = pl.read_excel(info_file, sheet_name="metadata")
+
+    data = pl.scan_csv(folder / "*.csv").collect().partition_by("source_file")
+
+    res_dfs: list[pl.DataFrame] = []
+    source_dfs: list[pl.DataFrame] = []
+
+    for df in data:
+        info = cast(
+            MetadataRow,
+            info_df.row(by_predicate=pl.col("cleaned_source_file") == df.item(0, "source_file"), named=True),
+        )
+        res_df, source_df = linear_fit(df, info)
+        res_dfs.append(res_df)
+        source_dfs.append(source_df)
+
+    return pl.concat(res_dfs), pl.concat(source_dfs)
